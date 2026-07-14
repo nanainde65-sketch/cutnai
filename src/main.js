562366a2d1de6e7f8c15e2e914be094d6ced6c90
nanainde65-sketch <nanainde65@gmail.com>
2026-07-14 15:02:13 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -24,14 +24,14 @@ document.querySelector('#app').innerHTML = `
     >
 
     <div class="buttons">
-      <button class="primary" id="uploadBtn">
-        Upload Video
-      </button>
+  <button class="primary" id="uploadBtn">
+    Upload Video
+  </button>
 
-      <button class="secondary">
-        Coba Demo
-      </button>
-    </div>
+  <button class="secondary" id="analyzeBtn">
+    Analisis AI
+  </button>
+</div>
 
    <div class="card">
   <h3>Status</h3>
@@ -42,6 +42,16 @@ document.querySelector('#app').innerHTML = `
 
   <video
     id="preview"
+    <div id="progressBox" style="display:none;margin-top:20px;">
+  <p id="progressText">🤖 AI sedang menganalisis...</p>
+
+  <progress
+    id="progressBar"
+    value="0"
+    max="100"
+    style="width:100%;">
+  </progress>
+</div>
     controls
     style="display:none;width:100%;margin-top:20px;border-radius:12px;">
   </video>
@@ -75,4 +85,41 @@ videoInput.onchange = () => {
     preview.src = URL.createObjectURL(file)
     preview.style.display = "block"
   }
+}
+const analyzeBtn = document.getElementById("analyzeBtn")
+const progressBox = document.getElementById("progressBox")
+const progressBar = document.getElementById("progressBar")
+const progressText = document.getElementById("progressText")
+
+analyzeBtn.onclick = () => {
+
+  if (videoInput.files.length === 0) {
+    alert("Pilih video terlebih dahulu.")
+    return
+  }
+
+  progressBox.style.display = "block"
+
+  let value = 0
+
+  const timer = setInterval(() => {
+
+    value += 5
+
+    progressBar.value = value
+
+    progressText.innerHTML =
+      "🤖 AI Menganalisis... " + value + "%"
+
+    if (value >= 100) {
+
+      clearInterval(timer)
+
+      progressText.innerHTML =
+        "✅ Analisis selesai."
+
+    }
+
+  }, 150)
+
 }
